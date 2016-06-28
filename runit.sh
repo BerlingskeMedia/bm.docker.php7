1213211e0410042437464b79047376f4b5325f2a
Casper Bruun <cbm@x32.dk>
2016-06-28 13:15:56 +0200

    Switch user and group ID for php-fpm. Use gosu to execute php cli as different user. Add ability to execute either php-fpm or php cli, remove supervisord. Do not use init script to start php process. DEVOPS-484

--- runit.sh
+++ runit.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
+set -e
 
-if [ -z "${USER_ID}" ]; then
-       USER_ID=1000
+USER_ID=${USER_ID:-"www-data"}
+GROUP_ID=${GROUP_ID:-$USER_ID}
+
+if [[ "$1" == 'php-fpm' || "$1" == 'php-fpm7.0' ]]; then
+    shift 1
+    echo "Running php-fpm7.0 $@"
+
+    # Set user and group for php-fpm process
+    if ! [ "$USER_ID" == 'www-data' ] ; then 
+        sed -i -e "s/^user = .*/user = $USER_ID/"    /etc/php/7.0/fpm/pool.d/symfony.pool.conf
+        sed -i -e "s/^group = .*/group = $GROUP_ID/" /etc/php/7.0/fpm/pool.d/symfony.pool.conf
+    fi
+
+    exec php-fpm7.0 "$@"
+
+elif [[ "$1" == 'php' || "$1" == 'php7.0' ]]; then 
+    shift 1
+    echo "Running cli: php7.0 $@"
+    exec gosu $USER_ID:$GROUP_ID php7.0 "$@"
+
+else 
+    echo "Running command: $@"
+    exec "$@"   
+
 fi
 
-usermod -u $USER_ID www-data
-groupmod -g $USER_ID www-data
 
-echo "Starting supervisor:"
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
-echo "Starting cron:"
-cron
-
-while true; do
-    /etc/init.d/php7.0-fpm start
-done
